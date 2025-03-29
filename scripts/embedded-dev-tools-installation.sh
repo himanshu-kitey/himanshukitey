eaab3bc1fcca80035815eab6077871d9db27fdd8
Himanshu-Kitey <himanshukitey@gmail.com>
2025-03-29 13:07:39 +0530

    Added Embedded-Dev-Tools-Installation Script

---
+++ scripts/embedded-dev-tools-installation.sh
@@ -0,0 +1,9 @@
+#installation of - sudo apt-get update
+#installation of - sudo apt-get upgrade
+#installation of gcc
+#installation of gdb
+#installation of g++
+#installation of make
+#installation of vim
+#installation of git
+
